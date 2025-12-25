4ee4ae17f2c20d86f4f4fba769ed447865465591
DarthEvgenius <clay14@yandex.ru>
2025-12-25 18:04:11 +0300

    hero page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
+  <div class="hero">
+    <NuxtImg
+      class="hero__image"
+      src="/images/qtim.png"
+      alt="qtim"
+      format="webp"
+      densities="1x 2x"
+      preload
+      fit="inside"
+      width="720"
+      height="214"
+    />
+
+    <NuxtLink
+      to="/posts"
+      class="hero__link"
+    >
+      QTIM — тестовое задание
+    </NuxtLink>
   </div>
 </template>
 
@@ -7,6 +25,25 @@
 
 </script>
 
-<style module lang='scss'>
+<style scoped>
+.hero {
+  padding-block: 3rem;
+  display: grid;
+  place-items: center;
+  gap: 8vw;
+}
 
+.hero__image {
+  width: 720px;
+  height: 214px;
+  max-width: 100%;
+}
+
+.hero__link {
+  transition: opacity 300ms ease;
+
+  &:hover {
+    opacity: 0.6;
+  }
+}
 </style>
